08ac690131ef4fa32bd98645a8bf861f11429bf3
larryeitel <larry@eitel.com>
2016-08-14 17:51:02 -0600

    refactored xdebugOff/On.bash

--- laradock/xdebugOff.bash
+++ laradock/xdebugOff.bash
@@ -1,23 +1,30 @@
 #! /bin/bash
 
+# NOTE: At the moment, this has only been confirmed to work PHP 7
+
 echo "STOP xdebug"
 
-REMOVE_EXTENSION_CMD='rm -f /usr/local/etc/php/conf.d/xdebug_extension_only.ini'
 
 # Grab full name of php-fpm container
 PHP_FPM_CONTAINER=$(docker-compose ps | grep php-fpm | cut -d" " -f 1)
+
+
+# Comment out xdebug extension line
+OFF_CMD="sed -i 's/^zend_extension=/;zend_extension=/g' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini"
+
 
 # If running on Windows, need to prepend with winpty :(
 if [[ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]]; then
     # This is the equivalent of:
     # winpty docker exec -it laradock_php-fpm_1 bash -c 'bla bla bla'
     # Thanks to @michaelarnauts at https://github.com/docker/compose/issues/593
-    winpty docker exec -it $PHP_FPM_CONTAINER bash -c "${REMOVE_EXTENSION_CMD}"
+    winpty docker exec -it $PHP_FPM_CONTAINER bash -c "${OFF_CMD}"
     docker restart $PHP_FPM_CONTAINER
     #docker-compose restart php-fpm
     winpty docker exec -it $PHP_FPM_CONTAINER bash -c 'php -v'
+
 else
-    docker exec -it $PHP_FPM_CONTAINER bash -c "${REMOVE_EXTENSION_CMD}"
+    docker exec -it $PHP_FPM_CONTAINER bash -c "${OFF_CMD}"
     # docker-compose restart php-fpm
     docker restart $PHP_FPM_CONTAINER
     docker exec -it $PHP_FPM_CONTAINER bash -c 'php -v'
--- laradock/xdebugOn.bash
+++ laradock/xdebugOn.bash
@@ -1,20 +1,32 @@
 #! /bin/bash
 
+# NOTE: At the moment, this has only been confirmed to work PHP 7
+
 echo "Start xdebug"
 
-
-COPY_INI_FILE_CMD='cp /var/www/laravel/laradock/workspace/xdebug_*.ini /usr/local/etc/php/conf.d'
 
 # Grab full name of php-fpm container
 PHP_FPM_CONTAINER=$(docker-compose ps | grep php-fpm | cut -d" " -f 1)
 
+
+# Copy important xdebug remote settings from workspace container to php-fpm
+# And uncomment line with xdebug extension, thus enabling it
+ON_CMD="cp \
+            /var/www/laravel/laradock/workspace/xdebug_settings_only.ini \
+            /usr/local/etc/php/conf.d \
+        && sed \
+            -i 's/^;zend_extension=/zend_extension=/g' \
+            /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini"
+
+
 # If running on Windows, need to prepend with winpty :(
 if [[ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]]; then
-    winpty docker exec -it $PHP_FPM_CONTAINER bash -c "${COPY_INI_FILE_CMD}"
+    winpty docker exec -it $PHP_FPM_CONTAINER bash -c "${ON_CMD}"
     docker restart $PHP_FPM_CONTAINER
     winpty docker exec -it $PHP_FPM_CONTAINER bash -c 'php -v'
+
 else
-    docker exec -it $PHP_FPM_CONTAINER bash -c "${COPY_INI_FILE_CMD}"
+    docker exec -it $PHP_FPM_CONTAINER bash -c "${ON_CMD}"
     docker restart $PHP_FPM_CONTAINER
     docker exec -it $PHP_FPM_CONTAINER bash -c 'php -v'
 fi
