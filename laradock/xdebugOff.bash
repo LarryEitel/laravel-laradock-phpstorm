2780494a41af431dd61183908b9d1b5c11b57650
larryeitel <larry@eitel.com>
2016-08-14 17:01:37 -0600

    refactored xdebugOff/On.bash

--- laradock/xdebugOff.bash
+++ laradock/xdebugOff.bash
@@ -4,20 +4,21 @@ echo "STOP xdebug"
 
 REMOVE_EXTENSION_CMD='rm -f /usr/local/etc/php/conf.d/xdebug_extension_only.ini'
 
+# Grab full name of php-fpm container
+PHP_FPM_CONTAINER=$(docker-compose ps | grep php-fpm | cut -d" " -f 1)
+
 # If running on Windows, need to prepend with winpty :(
 if [[ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]]; then
     # This is the equivalent of:
     # winpty docker exec -it laradock_php-fpm_1 bash -c 'bla bla bla'
     # Thanks to @michaelarnauts at https://github.com/docker/compose/issues/593
-    winpty docker exec -it $(docker-compose ps | grep php-fpm | cut -d" " -f 1) \
-        bash -c "${REMOVE_EXTENSION_CMD}"
-    docker-compose restart php-fpm
-    winpty docker exec -it $(docker-compose ps | grep php-fpm | cut -d" " -f 1) \
-        bash -c 'php -v'
+    winpty docker exec -it $PHP_FPM_CONTAINER bash -c "${REMOVE_EXTENSION_CMD}"
+    docker restart $PHP_FPM_CONTAINER
+    #docker-compose restart php-fpm
+    winpty docker exec -it $PHP_FPM_CONTAINER bash -c 'php -v'
 else
-    docker exec -it $(docker-compose ps | grep php-fpm | cut -d" " -f 1) \
-        bash -c "${REMOVE_EXTENSION_CMD}"
-    docker-compose restart php-fpm
-    docker exec -it $(docker-compose ps | grep php-fpm | cut -d" " -f 1) \
-        bash -c 'php -v'
+    docker exec -it $PHP_FPM_CONTAINER bash -c "${REMOVE_EXTENSION_CMD}"
+    # docker-compose restart php-fpm
+    docker restart $PHP_FPM_CONTAINER
+    docker exec -it $PHP_FPM_CONTAINER bash -c 'php -v'
 fi
--- laradock/xdebugOn.bash
+++ laradock/xdebugOn.bash
@@ -5,18 +5,16 @@ echo "Start xdebug"
 
 COPY_INI_FILE_CMD='cp /var/www/laravel/laradock/workspace/xdebug_*.ini /usr/local/etc/php/conf.d'
 
+# Grab full name of php-fpm container
+PHP_FPM_CONTAINER=$(docker-compose ps | grep php-fpm | cut -d" " -f 1)
 
 # If running on Windows, need to prepend with winpty :(
 if [[ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]]; then
-    winpty docker exec -it $(docker-compose ps | grep php-fpm | cut -d" " -f 1) \
-        bash -c "${COPY_INI_FILE_CMD}"
-    docker-compose restart php-fpm
-    winpty docker exec -it $(docker-compose ps | grep php-fpm | cut -d" " -f 1) \
-        bash -c 'php -v'
+    winpty docker exec -it $PHP_FPM_CONTAINER bash -c "${COPY_INI_FILE_CMD}"
+    docker restart $PHP_FPM_CONTAINER
+    winpty docker exec -it $PHP_FPM_CONTAINER bash -c 'php -v'
 else
-    docker exec -it $(docker-compose ps | grep php-fpm | cut -d" " -f 1) \
-        bash -c "${COPY_INI_FILE_CMD}"
-    docker-compose restart php-fpm
-    docker exec -it $(docker-compose ps | grep php-fpm | cut -d" " -f 1) \
-        bash -c 'php -v'
+    docker exec -it $PHP_FPM_CONTAINER bash -c "${COPY_INI_FILE_CMD}"
+    docker restart $PHP_FPM_CONTAINER
+    docker exec -it $PHP_FPM_CONTAINER bash -c 'php -v'
 fi
